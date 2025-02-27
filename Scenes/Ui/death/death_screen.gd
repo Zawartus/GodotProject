c003f6a9ac6bbd124113fe89e1eff18b9946a47a
Zawartus <stanleyekpl@gmail.com>
2025-02-28 00:28:26 +0100

    werske 0.026

--- Scenes/Ui/death/death_screen.gd
+++ Scenes/Ui/death/death_screen.gd
@@ -9,3 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_continue_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()	
